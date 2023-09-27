53d4dbfa8fa06bbb22a649acc418e19852390f32
Vickyvky13 <131648662+Vickyvky13@users.noreply.github.com>
2023-09-27 22:50:31 +0530

    Update inline.py

--- StringGen/utils/inline.py
+++ StringGen/utils/inline.py
@@ -9,7 +9,7 @@ keyboard = InlineKeyboardMarkup(
         [
             InlineKeyboardButton(text="sᴜᴘᴘᴏʀᴛ", url=SUPPORT_CHAT),
             InlineKeyboardButton(
-                text="ᴄʜᴀɴɴᴇʟ", url="https://t.me/AlishaUpdates"
+                text="ᴄʜᴀɴɴᴇʟ", url="https://t.me/solotreee"
             ),
         ],
     ]
