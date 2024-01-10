0bf6c824f66488c3df347da39120d873f77eefc0
Vickyvky13 <131648662+Vickyvky13@users.noreply.github.com>
2024-01-11 00:09:37 +0530

    Update start.py

--- StringGen/modules/start.py
+++ StringGen/modules/start.py
@@ -3,7 +3,20 @@ from pyrogram import filters
   
  from StringGen import Anony 
  from StringGen.utils import add_served_user, keyboard 
+
+ force_btn = InlineKeyboardMarkup([ 
+     [InlineKeyboardButton(text="🍁ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ🍂", url="t.me/solotreee")], 
+     [InlineKeyboardButton(text="〄╏ᴄʜᴀᴛ ɢᴘᴛ╏ᴀɪ🧞", url="t.me/Chatgptasking_bot")]]) 
   
+ async def check_is_joined(message, Message, client):     
+     try: 
+         userid = message.from_user.id 
+         user_name = message.from_user.first_name 
+         status = await app.get_chat_member("solotreee", userid) 
+         return True 
+     except Exception: 
+         await message.reply_text(f'**🚧 ғɪʀsᴛ ᴊᴏɪɴ ᴛʜᴇ ʙᴏᴛ ᴄʜᴀɴɴᴇʟ ⚠️**\n┉───┈┈╌╍╌┄┈───┉┉───┈┈╌\n⌯︙**W⃟ᴇʟᴄᴏᴍᴇ :[{message.from_user.first_name}](tg://user?id={message.from_user.id})**\n⌯︙🏝️ **ʙᴏᴛ ᴄʜᴀɴɴᴇʟ :** [S⃟ᴏʟᴏ ᴛʀᴇᴇ](https://t.me/solotreee)\n┉───┈┈╌╍╌┄┈───┉┉───┈┈╌',reply_markup=force_btn,parse_mode="markdown",disable_web_page_preview=False) 
+         return False 
   
  @Anony.on_message(filters.command("start") & filters.private & filters.incoming) 
  async def f_start(_, message: Message): 
