6c3b6cea8e2788a604cf9e1d189e48f9e90d2842
Vickyvky13 <131648662+Vickyvky13@users.noreply.github.com>
2024-01-11 00:55:03 +0530

    Create filters.py

---
+++ StringGen/filters.py
@@ -0,0 +1,11 @@
+# filters.py
+
+def incoming(message):
+    # Define your filter logic here
+    pass
+
+def private(message):
+    # Define your filter logic here
+    pass
+
+# ... other filter functions if needed
