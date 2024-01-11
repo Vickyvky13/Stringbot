bc1aa0ff417a6b2e96dedcb491cfcfd2751799da
Vickyvky13 <131648662+Vickyvky13@users.noreply.github.com>
2024-01-11 18:35:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,6 +12,6 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
 OWNER_ID = int(getenv("OWNER_ID", 1356469075))
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/solotreee")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/solotree_support")
 MUST_JOIN= getenv("MUST_JOIN","https://t.me/solotreee")
 RESTART_BOT= getenv("RESTART_BOT", "https://t.me/Solostringsession_bot?start=help")
