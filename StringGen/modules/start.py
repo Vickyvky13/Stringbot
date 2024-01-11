9448ceeb6edf6285de84ddf0631bd46615e3c7bc
Vickyvky13 <131648662+Vickyvky13@users.noreply.github.com>
2024-01-11 18:34:40 +0530

    Update start.py

--- StringGen/modules/start.py
+++ StringGen/modules/start.py
@@ -7,7 +7,7 @@ from StringGen.utils import add_served_user, keyboard
 @Anony.on_message(filters.command("start") & filters.private & filters.incoming)
 async def f_start(_, message: Message):
     await message.reply_text(
-        text=f"⌯ ʜᴇʏ {message.from_user.mention} 🥀\n\n⌯ ꜰʀɪꜱᴛ ᴊᴏɪɴ [ᴄʜᴀɴɴᴇʟ](https://t.me/solotreee/141) & [ꜱᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ](https://t.me/solotree_support).\n\n⌯ ᴛʜɪꜱ ɪꜱ {Anony.mention}, ᴀᴅᴠᴀɴᴄᴇ ꜱᴛʀɪɴɢ ꜱᴇꜱꜱɪᴏɴ ʙᴏᴛ ɴᴏ ɴᴇᴇᴅᴇᴅ ꜰᴏʀ ||<b><u>ᴀᴘᴋ_ɪᴅ</u></b>|| & ||<b><u>ᴀᴘɪ_ʜᴀꜱʜ</b></u>|| | ᴘᴜᴛ ʏᴏᴜʀ ᴍᴏʙɪʟᴇ ɴᴜᴍʙᴇʀ ɢᴇᴛ ʏᴏᴜʀ ꜱᴇꜱꜱɪᴏɴ ꜰᴀꜱᴛʟʏ.",
+        text=f"⌯ ʜᴇʏ {message.from_user.mention} 🥀\n\n⌯ ᴛʜɪꜱ ɪꜱ {Anony.mention}, ᴀᴅᴠᴀɴᴄᴇ ꜱᴛʀɪɴɢ ꜱᴇꜱꜱɪᴏɴ ʙᴏᴛ ɴᴏ ɴᴇᴇᴅᴇᴅ ꜰᴏʀ ||<b><u>ᴀᴘᴋ_ɪᴅ</u></b>|| & ||<b><u>ᴀᴘɪ_ʜᴀꜱʜ</b></u>|| | ᴘᴜᴛ ʏᴏᴜʀ ᴍᴏʙɪʟᴇ ɴᴜᴍʙᴇʀ ɢᴇᴛ ʏᴏᴜʀ ꜱᴇꜱꜱɪᴏɴ ꜰᴀꜱᴛʟʏ.",
         reply_markup=keyboard,
         disable_web_page_preview=True,
     )
